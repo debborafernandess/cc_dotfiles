0a2be8ac4ac3f200c9d03447af3f78e5a6d3bcac
Débora <debborafernandess@gmail.com>
2017-03-20 18:58:19 -0300

    Add my github to install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.cc_dotfiles" ]
 then
-  echo "Installing Campus Code Dotfiles"
+  echo "Installing Dotfiles"
   echo "We'll install:"
   echo "  - tmux"
   echo "  - silver searcher"
@@ -12,9 +12,9 @@ then
   echo "  - iterm2 (mac)"
   echo "  - atom"
 
-  git clone --depth=10 https://github.com/campuscode/cc_dotfiles.git "$HOME/.cc_dotfiles"
+  git clone --depth=10 https://github.com/debborafernandess/cc_dotfiles.git "$HOME/.cc_dotfiles"
   cd "$HOME/.cc_dotfiles"
   rake install
 else
-  echo "You already have Campus Code Dotfiles installed."
+  echo "You already have Dotfiles installed."
 fi
